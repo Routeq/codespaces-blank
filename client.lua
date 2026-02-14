425199e0262733a24d1d2ea8b14bd8bdc87f7bd1
Routeq <infamousroute@gmail.com>
2026-02-14 22:17:16 +0000

    Last fix

--- client.lua
+++ client.lua
@@ -1,24 +1,24 @@
 local hasClosed = false
 
 local function closeLoadingScreen()
-    if hasClosed then
-        return
-    end
-    hasClosed = true
-    SendNUIMessage({ action = "close" })
-    ShutdownLoadingScreenNui()
-    ShutdownLoadingScreen()
+	if hasClosed then
+		return
+	end
+	hasClosed = true
+	SendNUIMessage({ action = "close" })
+	ShutdownLoadingScreenNui()
+	ShutdownLoadingScreen()
 end
 
 AddEventHandler("playerSpawned", function()
-    closeLoadingScreen()
+	closeLoadingScreen()
 end)
 
 AddEventHandler("onClientResourceStart", function(resourceName)
-    if resourceName ~= GetCurrentResourceName() then
-        return
-    end
-    if NetworkIsSessionStarted() then
-        closeLoadingScreen()
-    end
+	if resourceName ~= GetCurrentResourceName() then
+		return
+	end
+	if NetworkIsSessionStarted() then
+		closeLoadingScreen()
+	end
 end)
