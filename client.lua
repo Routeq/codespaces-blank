bd5b69a4711fc0309f5b295a30591292a30f8400
Routeq <infamousroute@gmail.com>
2026-02-14 22:17:07 +0000

    Last Lap

---
+++ client.lua
@@ -0,0 +1,24 @@
+local hasClosed = false
+
+local function closeLoadingScreen()
+    if hasClosed then
+        return
+    end
+    hasClosed = true
+    SendNUIMessage({ action = "close" })
+    ShutdownLoadingScreenNui()
+    ShutdownLoadingScreen()
+end
+
+AddEventHandler("playerSpawned", function()
+    closeLoadingScreen()
+end)
+
+AddEventHandler("onClientResourceStart", function(resourceName)
+    if resourceName ~= GetCurrentResourceName() then
+        return
+    end
+    if NetworkIsSessionStarted() then
+        closeLoadingScreen()
+    end
+end)
---
+++ fivem-resource/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version("cerulean")
+
+game("gta5")
+
+ui_page("index.html")
+
+files({
+	"index.html",
+	"assets/**",
+})
